b1ef2085a917149e2e0b611b091003f2c3c995f1
RealTacticalPanda <tacticalpanda420@gmail.com>
2025-07-21 11:10:09 -0500

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -39,7 +39,7 @@ $(document).ready(function () {
 
     // <!-- emailjs to mail contact form data -->
     $("#contact-form").submit(function (event) {
-        emailjs.init("ko0zw0u1EYp7JNxxZ");
+        emailjs.init("DAnGNAwxraZTeNMzn");
 
         emailjs.sendForm('contact_service', 'template_contact', '#contact-form')
             .then(function (response) {
